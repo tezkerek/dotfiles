22a04d2e68e8dd023c7712f85a8bd7e369dbaf99
tezkerek <andrei.ancuta@gmail.com>
2026-05-17 20:43:56 +0300

    nvim: remove plenary dependency

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -69,10 +69,8 @@ return packer.startup(function(use)
     -- Integration
     use('christoomey/vim-tmux-navigator')
     use('tpope/vim-fugitive')
-    use('nvim-lua/plenary.nvim')
     use {
         'lewis6991/gitsigns.nvim',
-        requires = 'nvim-lua/plenary.nvim',
         config = function()
             require('plugins/gitsigns')
         end,
